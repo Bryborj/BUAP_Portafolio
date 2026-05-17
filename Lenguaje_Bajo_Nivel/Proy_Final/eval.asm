49bc5ea882e535e4133fb6c6602c44192270b103
Bryan Borges <root.brybor.dev@gmail.com>
2026-05-16 23:48:02 -0600

    feat: implement evaluation logic in eval.asm and add build and cleanup batch scripts

--- Lenguaje_Bajo_Nivel/Proy_Final/eval.asm
+++ Lenguaje_Bajo_Nivel/Proy_Final/eval.asm
@@ -2,7 +2,9 @@
 .STACK 100H
 .DATA
     msgInit DB 'Ingresa tu calificacion (0-9): $'
-
+    msgAprobado DB 'Alumno aprobado $
+    msgNoAprobado DB 'Alumno reprobado $'
+    msgContinuar DB '¿Deseas evaluar otra calificacion? (S/N): $'
 .CODE
 MAIN PROC
     MOV AX, @DATA
@@ -17,4 +19,37 @@ MAIN PROC
 
     SUB AL, 30H
 
+    CMP AL, 6
+    JG APROBADO
+    JE APROBADO
+    JL NO_APROBADO
+
+APROBADO:
+    LEA DX, msgAprobado
+    MOV AH, 09H
+    INT 21H
+
+    JMP CONTINUAR
+
+NO_APROBADO:
+    LEA DX, msgNoAprobado
+    MOV AH, 09H
+    INT 21H
+
+    JMP CONTINUAR
+
+CONTINUAR:
+    LEA DX, msgContinuar
+    MOV AH, 09H
+    INT 21H
+
+    CMP AL, 'S'
+    JE INICIO
+    CMP AL, 'N'
+    JE FIN
+
+
+FIN:
+    MOV AH, 4CH
+    INT 21H
 END MAIN
