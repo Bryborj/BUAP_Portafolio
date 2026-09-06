77867abe13bc8791e158e9a46dbdaa14198130dd
Bryan Borges <root.brybor.dev@gmail.com>
2026-09-06 17:57:11 -0600

    refactor: update tile module import to tiles_Struct_Get and add debug trigger

--- Graficacion/Parcial_1/Proyecto_Evaluacion-1/Proyecto_Eva_1.py
+++ Graficacion/Parcial_1/Proyecto_Evaluacion-1/Proyecto_Eva_1.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import tiles as t
+import tiles_Struct_Get as tg
 
 # maps of the tiles
 map = np.zeros((40, 40))  # 40 x 40 tiles, all initially 0
+
+tg.DEBUG()
